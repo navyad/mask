93db5da42a449f94c20dd22d35738cf65ccf68b4
navyad <navyad.pro@gmail.com>
2023-04-14 13:08:50 +0530

    test and flake8 fix

--- mask.py
+++ mask.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class MaskType(Enum):
     EMAIL = "email"
 
@@ -7,5 +8,5 @@ class MaskType(Enum):
 def mask(mask_type: MaskType, val: str) -> str:
     if not isinstance(mask_type, MaskType):
         raise Exception("Invalid masktype: expected MaskType")
-    if not instance(val, str) or val == "":
+    if not isinstance(val, str) or val == "":
         raise Exception("Invalid val: expected string")
--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,4 +1,3 @@
-
 import pytest
 from mask import mask, MaskType
 
@@ -6,7 +5,10 @@ from mask import mask, MaskType
 def test_invalid_mask_type():
     with pytest.raises(Exception) as exc_info:
         mask(1, "abc")
+    assert exc_info.value.args[0] == "Invalid masktype: expected MaskType"
+
 
 def test_invalid_val():
     with pytest.raises(Exception) as exc_info:
-        mask(MaskType.EMAIL, "abc")
+        mask(MaskType.EMAIL, 111)
+    assert exc_info.value.args[0] == "Invalid val: expected string"
